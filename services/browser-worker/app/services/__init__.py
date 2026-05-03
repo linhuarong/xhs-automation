8e08ddfbe14b4c9a55ae6c319aa60bb3172245f6
Yin <linhuarong@edisplayblock.com>
2026-05-03 14:40:50 +0800

    export image downloader service

--- services/browser-worker/app/services/__init__.py
+++ services/browser-worker/app/services/__init__.py
@@ -1,3 +1,10 @@
+from app.services.image_downloader import ImageDownloadResult, download_images
 from app.services.job_registry import JobRegistry, JobStatus, job_registry
 
-__all__ = ["JobRegistry", "JobStatus", "job_registry"]
+__all__ = [
+    "ImageDownloadResult",
+    "download_images",
+    "JobRegistry",
+    "JobStatus",
+    "job_registry",
+]
