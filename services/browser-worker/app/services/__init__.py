209050a3967a838b7a56ca254356821956ee30c5
Yin <linhuarong@edisplayblock.com>
2026-05-03 15:10:56 +0800

    export browser automation utility services

--- services/browser-worker/app/services/__init__.py
+++ services/browser-worker/app/services/__init__.py
@@ -1,9 +1,11 @@
 from app.services.image_downloader import ImageDownloadResult, download_images
 from app.services.job_registry import JobRegistry, JobStatus, job_registry
+from app.services.screenshot_service import save_screenshot
 
 __all__ = [
     "ImageDownloadResult",
     "download_images",
+    "save_screenshot",
     "JobRegistry",
     "JobStatus",
     "job_registry",
